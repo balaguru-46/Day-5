a94b215c9a9fb92d323f7fc94a56feac7443ad14
balaguru-46 <balagurubg219@gmail.com>
2026-07-24 11:10:44 +0530

    Time: 74 ms (53.29%) | Memory: 63.4 MB (40.60%) - LeetSync

---
+++ 2758-check-if-object-instance-of-class/check-if-object-instance-of-class.js
@@ -0,0 +1,22 @@
+/**
+ * @param {*} obj
+ * @param {*} classFunction
+ * @return {boolean}
+ */
+var checkIfInstanceOf = function(obj, classFunction) {
+    if (obj == null || typeof classFunction !== "function") {
+        return false;
+    }
+
+    let prototype = Object.getPrototypeOf(Object(obj));
+
+    while (prototype !== null) {
+        if (prototype === classFunction.prototype) {
+            return true;
+        }
+
+        prototype = Object.getPrototypeOf(prototype);
+    }
+
+    return false;
+};
